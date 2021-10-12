d59654c8cf0f6efc545b75b5604ac28ee0c6b5f4
chinsp <upmho@student.kit.edu>
2021-10-12 21:07:38 +0200

    minor edit

--- VlasovPoissonProbs.py
+++ VlasovPoissonProbs.py
@@ -1,12 +1,13 @@
 # Defining the Vlasov-Poisson problem for flow of particles with their corresponding K-step, S-step and L-step
 
-def K_step():
-    return 1.0
+def K_step(K, V):
+
+    return 0.0
 
 
-def S_step():
-    return 1.0
+def S_step(X, S, V):
+    return 0.0
 
 
-def L_step():
-    return 1.0
+def L_step(X, L):
+    return 0.0
