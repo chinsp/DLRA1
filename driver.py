cc6ce22b48ecdfd185f855881b7f18466918c698
chinsp <upmho@student.kit.edu>
2021-12-01 11:34:10 +0100

    added strang splitting driver

--- driver.py
+++ driver.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 from numpy.linalg import svd, qr
 from Lubich_paper import K_step, S_step, L_step, A
 
-# First order Lie-Trotter splitting.
+
 # We project the RHS of the problem onto a low rank manifold.
 # We Have the following split: P(f) g = P_{\bar{V}} g - P_{\bar{V}} P_{\bar{X}} g + P_{\bar{X}} g
 
+# First order Lie-Trotter splitting.
+
 
 def KSL_Lie(t, X0, S0, V0):
-    #Computing time increment
-    dA = A(t) - A(t-1)
+    # Computing time increment
+    dA = A(t) - A(t - 1)
 
     # K-step
     K0 = X0.dot(S0)
@@ -30,7 +32,10 @@ def KSL_Lie(t, X0, S0, V0):
     return X1, S1.T, V1
 
 
-def LSK_Lie(X0, S0, V0):
+def LSK_Lie(t, X0, S0, V0):
+    # Computing time increment
+    dA = A(t) - A(t - 1)
+
     # L-step
     L0 = S0.dot(V0.T)
     L1 = L_step(X0, L0)
@@ -51,5 +56,35 @@ def LSK_Lie(X0, S0, V0):
     return X1, S1, V1
 
 
+# Second order Strang splitting scheme
 
+def KSL_Strang(t, X0, S0, V0):
+    # Defining A for time t0 ,t_1/2 and t1
+    A0, A_mid, A1 = A(t - 1), A(t - 1 / 2), A(t)
 
+    # half K-step t0 -> t_1/2
+    K0 = X0.dot(S0)
+    K_mid = K_step(K0, V0, A_mid - A0)
+    # QR decomposition
+    X_mid1, S_mid1 = qr(K_mid)
+
+    # half S-step t0 -> t_1/2
+    S_mid2 = S_step(X_mid1, S_mid1, V0, A_mid - A0)
+
+    # full L-step t0 -> t1
+    L0 = S_mid2.dot(V0.T)
+    L1 = L_step(L0, X_mid1, A1 - A0)
+    # QR decomposition
+    V1, S_mid3 = qr(L1)
+    S_mid3 = S_mid3.T
+
+    # half S-step t_1/2 -> t1
+    S_mid4 = S_step(X_mid1, S_mid3, V1, A1 - A_mid)
+
+    # half K-step t_1/2 -> t1
+    K0 = X_mid1.dot(S_mid4)
+    K1 = K_step(K0, V1, A1 - A_mid)
+    # QR decomposition
+    X1, S1 = qr(K_mid)
+
+    return X1, S1, V1
