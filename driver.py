54bf0dc0775829ba324082d114cd107cc03ec8f2
chinsp <upmho@student.kit.edu>
2021-12-01 12:18:52 +0100

    added proper time increments

--- driver.py
+++ driver.py
@@ -10,9 +10,9 @@ from Lubich_paper import K_step, S_step, L_step, A
 # First order Lie-Trotter splitting.
 
 
-def KSL_Lie(t, X0, S0, V0):
+def KSL_Lie(t, dt, X0, S0, V0):
     # Computing time increment
-    dA = A(t) - A(t - 1)
+    dA = A(t) - A(t - dt)
 
     # K-step
     K0 = X0.dot(S0)
@@ -32,9 +32,9 @@ def KSL_Lie(t, X0, S0, V0):
     return X1, S1.T, V1
 
 
-def LSK_Lie(t, X0, S0, V0):
+def LSK_Lie(t, dt, X0, S0, V0):
     # Computing time increment
-    dA = A(t) - A(t - 1)
+    dA = A(t) - A(t - dt)
 
     # L-step
     L0 = S0.dot(V0.T)
@@ -58,32 +58,33 @@ def LSK_Lie(t, X0, S0, V0):
 
 # Second order Strang splitting scheme
 
-def KSL_Strang(t, X0, S0, V0):
-    # Defining A for time t0 ,t_1/2 and t1
-    A0, A_mid, A1 = A(t - 1), A(t - 1 / 2), A(t)
+def KSL_Strang(t, dt, X0, S0, V0):
+    # Defining increment of A for time t0 ,t_1/2 and t1
+    A0, A_mid, A1 = A(t - dt), A(t - dt / 2), A(t)
+    dA_mid1, dA_mid2 = A_mid - A0, A1 - A_mid
 
     # half K-step t0 -> t_1/2
     K0 = X0.dot(S0)
-    K_mid = K_step(K0, V0, A_mid - A0)
+    K_mid = K_step(K0, V0, dA_mid1)
     # QR decomposition
     X_mid1, S_mid1 = qr(K_mid)
 
     # half S-step t0 -> t_1/2
-    S_mid2 = S_step(X_mid1, S_mid1, V0, A_mid - A0)
+    S_mid2 = S_step(X_mid1, S_mid1, V0, dA_mid1)
 
     # full L-step t0 -> t1
     L0 = S_mid2.dot(V0.T)
-    L1 = L_step(L0, X_mid1, A1 - A0)
+    L1 = L_step(L0, X_mid1, dA_mid2 + dA_mid1)
     # QR decomposition
     V1, S_mid3 = qr(L1)
     S_mid3 = S_mid3.T
 
     # half S-step t_1/2 -> t1
-    S_mid4 = S_step(X_mid1, S_mid3, V1, A1 - A_mid)
+    S_mid4 = S_step(X_mid1, S_mid3, V1, dA_mid2)
 
     # half K-step t_1/2 -> t1
     K0 = X_mid1.dot(S_mid4)
-    K1 = K_step(K0, V1, A1 - A_mid)
+    K1 = K_step(K0, V1, dA_mid2)
     # QR decomposition
     X1, S1 = qr(K_mid)
 
